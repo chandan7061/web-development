91fae4f4e0e2d6f15509aa7721dca8c22b7f2d4d
chandan7061 <chandan9507874329@gmail.com>
2026-05-12 18:14:54 +0530

    Add To Do List application with HTML, CSS, and JavaScript functionality

---
+++ Project/To-Do-List/script.js
@@ -0,0 +1,48 @@
+function AddTask() {
+  
+  const taskInput =
+  document.getElementById("taskInput");
+
+  const task =
+  taskInput.value.trim();
+
+  if (task === "") {
+    return;
+  }
+
+  const taskList =
+  document.getElementById("taskList");
+
+  const li =
+  document.createElement("li");
+
+  li.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+
+  li.innerHTML = `
+
+    <span>
+      ${task}
+    </span>
+
+    <button
+      class="btn btn-outline-danger"
+      onclick="DeleteTask(this)"
+    >
+
+      <i class="bi bi-trash"></i>
+
+      Delete
+
+    </button>
+  `;
+
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+}
+
+function DeleteTask(button) {
+
+  button.parentElement.remove();
+}
