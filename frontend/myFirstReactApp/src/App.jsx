936eb0d9c8e29c3ee3588de364b41962885f46c8
chandan7061 <chandan9507874329@gmail.com>
2026-06-03 19:34:37 +0530

    Add routing and new pages: Home, About, Product, Contact Us, Login, and Register; update Header component for navigation

--- frontend/myFirstReactApp/src/App.jsx
+++ frontend/myFirstReactApp/src/App.jsx
@@ -1,23 +1,27 @@
-//  import React from "react";
-
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { FaWifi } from "react-icons/fa";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div id="abcd">My First React App</div>
-      <p className="bg-success">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil tempore
-        architecto cupiditate adipisci veritatis voluptas rerum nemo, est quia
-        quasi. Beatae ducimus rerum temporibus ipsum aspernatur laborum? Libero,
-        explicabo dolorum.
-      </p>
-      <button>click here</button>
-
-      <Footer />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/myFirstReactApp/src/pages/Product.jsx
@@ -0,0 +1,10 @@
+function Product() {
+  return (
+    <div>
+      <h1>Product</h1>
+      <p>This is the Product page</p>
+    </div>
+  );
+}
+
+export default Product;
