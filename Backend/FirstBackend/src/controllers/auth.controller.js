d79ecde5ea05125515ac9a4c2decbac33c2fd205
chandan7061 <chandan9507874329@gmail.com>
2026-06-25 18:54:19 +0530

    feat: Implement user login validation and error handling in auth controller

--- Backend/FirstBackend/src/controllers/auth.controller.js
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -1,5 +1,19 @@
- export const loginUser = (req, res) => {
-  res.json({ message: "Login successful from controller" });
+export const loginUser =async (req, res) => {
+  try {
+    const { fullName, phone, password, email, gender, dob } = req.body;
+    if (!fullName || !phone || !password || !email || !gender || !dob) {
+      res.status(400).json({ message: "All fields are required" });
+      return;
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(409).json({ message: "User already exists" });
+      return;
+    }
+
+
+  } catch (error) {}
 };
 
 export const logoutUser = (req, res) => {
@@ -8,4 +22,4 @@ export const logoutUser = (req, res) => {
 
 export const registerUser = (req, res) => {
   res.json({ message: "Registration successful from controller" });
-}
+};
